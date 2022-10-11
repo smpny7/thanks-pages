a462c25b7bebb331d22e8b876be77b7a5b044272
smpny7 <git@coalabo.net>
2022-10-11 22:46:30 +0900

    feat: Google Tag Manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import NextDocument, { Head, Html, Main, NextScript } from "next/document";
 import Script from "next/script";
 import { config } from "../lib/config";
 
@@ -33,25 +33,20 @@ class Document extends NextDocument<Props> {
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap&text=岡山大学祭201は終了しまたくさんのご来場ありがとうざい企画:校友会・実行委員制作電子計算機研究こちら"
             rel="stylesheet"
           />
-          {process.env.NEXT_PUBLIC_GA_TRACKING_ID && (
-            <>
-              <Script
-                strategy="afterInteractive"
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
-              />
-              <Script
-                id="gtm"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                  __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}');
+          {process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID && (
+            <Script
+              id="gtm"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}');
                   `,
-                }}
-              />
-            </>
+              }}
+            />
           )}
         </Head>
         <body style={{ backgroundColor: config.backgroundColor }}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,14 @@ import classNames from "classnames";
 import { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { config } from "../lib/config";
+
+declare global {
+  interface Window {
+    dataLayer: Record<string, unknown>[];
+  }
+}
 
 const Home: NextPage = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -79,6 +85,12 @@ const Home: NextPage = () => {
 
             <a
               href="https://www.okayama-u.ac.jp/tp/event/event_id2979.html"
+              onClick={() =>
+                window.dataLayer.push({
+                  event: "fetch_random_images",
+                  fetch_random_images_trigger: "a_tag",
+                })
+              }
               className="bg-primary flex items-center justify-between h-14 sm:h-[77px] w-full lg:w-[422px] pl-6 sm:pl-10 pr-4 sm:pr-8 drop-shadow-[4px_4px_0_black] md:drop-shadow-[5px_5px_0_black]"
             >
               <span className="text-white font-bold sm:text-2xl md:text-[26px] tracking-wider">
