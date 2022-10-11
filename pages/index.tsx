bc8b5ef6e022c295cfa7d58b5373376efdc9cff6
smpny7 <git@coalabo.net>
2022-10-11 19:45:48 +0900

    perf: Lighthouse アクセシビリティ改善

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,15 +43,21 @@ const Home: NextPage = () => {
       <div className="absolute bottom-0 left-0 right-0 top-0 flex flex-col">
         <div className="grow px-8 w-full max-w-[1400px] mx-auto flex items-center">
           <div className="w-full pt-10">
-            <h1 className="text-white/90 tracking-wider text-[40px] sm:text-[50px] md:text-[60px] font-bold drop-shadow-[3px_3px_0_rgba(0,0,0,0.75)] md:drop-shadow-[5px_5px_0_rgba(0,0,0,0.75)]">
+            <h1
+              className="text-white/90 tracking-wider text-[40px] sm:text-[50px] md:text-[60px] font-bold"
+              style={{ textShadow: "0 0 15px black" }}
+            >
               岡山大学祭2021は
               <br />
               終了しました
             </h1>
 
             <div className="mt-24 flex justify-end items-center">
-              <div className="bg-white/90 w-1/3 sm:w-1/2 md:w-[320px] h-[2px] drop-shadow-[2px_2px_0_rgba(0,0,0,0.75)] md:drop-shadow-[3.5px_3.5px_0_rgba(0,0,0,0.75)] mr-2" />
-              <p className="text-white/90 tracking-wider sm:text-2xl md:text-[28px] text-right font-bold !leading-7 sm:!leading-10 md:!leading-[3rem] drop-shadow-[2px_2px_0_rgba(0,0,0,0.75)] md:drop-shadow-[3.5px_3.5px_0_rgba(0,0,0,0.75)]">
+              <div className="bg-white/90 w-1/3 sm:w-1/2 md:w-[320px] h-[2px] drop-shadow-[2px_2px_0_rgba(0,0,0,0.5)] md:drop-shadow-[4px_4px_0_rgba(0,0,0,0.5)] mr-2" />
+              <p
+                className="text-white/90 tracking-wider sm:text-2xl md:text-[28px] text-right font-bold !leading-7 sm:!leading-10 md:!leading-[3rem]"
+                style={{ textShadow: "0 0 15px black" }}
+              >
                 たくさんのご来場
                 <br />
                 ありがとうございました
@@ -75,10 +81,10 @@ const Home: NextPage = () => {
               href="https://www.okayama-u.ac.jp/tp/event/event_id2979.html"
               className="bg-primary flex items-center justify-between h-14 sm:h-[77px] w-full lg:w-[422px] pl-6 sm:pl-10 pr-4 sm:pr-8 drop-shadow-[4px_4px_0_black] md:drop-shadow-[5px_5px_0_black]"
             >
-              <span className="text-white font-bold sm:text-2xl md:text-[26px] tracking-wider drop-shadow-[2px_2px_0_rgba(255,255,255,0.1)] md:drop-shadow-[4px_4px_0_rgba(255,255,255,0.1)]">
+              <span className="text-white font-bold sm:text-2xl md:text-[26px] tracking-wider">
                 学祭2022はこちら
               </span>
-              <div className="relative w-7 sm:w-[45px] pt-1 drop-shadow-[2px_2px_0_rgba(255,255,255,0.25)] md:drop-shadow-[3px_3px_0_rgba(255,255,255,0.25)]">
+              <div className="relative w-7 sm:w-[45px] pt-1">
                 <Image
                   src="/arrow-light.svg"
                   height={29}
