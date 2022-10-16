eebf96ba7e3dc659413600e7ecc94b45ec8bae25
smpny7 <git@coalabo.net>
2022-10-16 23:01:46 +0900

    fix: 翻訳修正

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,6 +1,6 @@
 const text = {
   TITLE: "Okayama University Festival\n2021 has ended",
-  SUB_TITLE: "We deeply appreciate\nthat we had lots of attendance.",
+  SUB_TITLE: "We deeply appreciate\nwe had lots of attendance.",
   PLANNER: "Alumni Association / School Festival Executive Committee",
   PRODUCER: "Produced by Okayama University Computer Research Club",
   BUTTON_LABEL: "School Festival 2022",
