40171e3a317f0de74f40c2214bc56188d62cc511
smpny7 <git@coalabo.net>
2022-10-16 23:21:25 +0900

    feat: Tailwind Sort

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import classNames from "classnames";
 import { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useLocale } from "../hooks/useLocale";
 
@@ -25,7 +24,7 @@ const Home: NextPage = () => {
 
       <div
         className={classNames(
-          "movie overflow-hidden h-screen w-screen relative duration-700",
+          "movie relative h-screen w-screen overflow-hidden duration-700",
           isLoading ? "opacity-0" : "opacity-100"
         )}
       >
@@ -44,10 +43,10 @@ const Home: NextPage = () => {
       </div>
 
       <div className="absolute bottom-0 left-0 right-0 top-0 flex flex-col">
-        <div className="grow px-8 w-full max-w-[1400px] mx-auto flex items-center relative">
+        <div className="relative mx-auto flex w-full max-w-[1400px] grow items-center px-8">
           <div className="w-full pt-10">
             <h1
-              className="text-white/90 tracking-wider text-[40px] sm:text-[50px] md:text-[60px] font-bold whitespace-pre-wrap duration-500"
+              className="whitespace-pre-wrap text-[40px] font-bold tracking-wider text-white/90 duration-500 sm:text-[50px] md:text-[60px]"
               style={{ textShadow: "0 0 15px black" }}
             >
               {t.TITLE}
@@ -55,13 +54,13 @@ const Home: NextPage = () => {
 
             <div
               className={classNames(
-                "flex justify-end items-center",
+                "flex items-center justify-end",
                 locale === "jp" ? "mt-24" : "mt-10 md:mt-24"
               )}
             >
-              <div className="bg-white/90 w-1/3 sm:w-1/2 md:w-[320px] h-[2px] drop-shadow-[2px_2px_0_rgba(0,0,0,0.5)] md:drop-shadow-[4px_4px_0_rgba(0,0,0,0.5)] mr-2" />
+              <div className="mr-2 h-[2px] w-1/3 bg-white/90 drop-shadow-[2px_2px_0_rgba(0,0,0,0.5)] sm:w-1/2 md:w-[320px] md:drop-shadow-[4px_4px_0_rgba(0,0,0,0.5)]" />
               <p
-                className="text-white/90 tracking-wider whitespace-pre-wrap sm:text-2xl md:text-[28px] text-right font-bold !leading-7 sm:!leading-10 md:!leading-[3rem]"
+                className="whitespace-pre-wrap text-right font-bold !leading-7 tracking-wider text-white/90 sm:text-2xl sm:!leading-10 md:text-[28px] md:!leading-[3rem]"
                 style={{ textShadow: "0 0 15px black" }}
               >
                 {t.SUB_TITLE}
@@ -70,21 +69,21 @@ const Home: NextPage = () => {
           </div>
         </div>
 
-        <div className="flex-none h-28 sm:h-40 bg-white">
-          <div className="w-full px-8 max-w-[1400px] mx-auto flex items-center justify-between h-full">
-            <div className="hidden lg:block space-y-2">
-              <p className="text-text text-lg font-bold">{t.PLANNER}</p>
-              <p className="text-text text-lg font-bold">{t.PRODUCER}</p>
+        <div className="h-28 flex-none bg-white sm:h-40">
+          <div className="mx-auto flex h-full w-full max-w-[1400px] items-center justify-between px-8">
+            <div className="hidden space-y-2 lg:block">
+              <p className="text-lg font-bold text-text">{t.PLANNER}</p>
+              <p className="text-lg font-bold text-text">{t.PRODUCER}</p>
             </div>
 
             <a
               href="https://www.okayama-u.ac.jp/tp/event/event_id2979.html"
-              className="bg-primary flex items-center justify-between h-14 sm:h-[77px] w-full lg:w-[422px] pl-6 sm:pl-10 pr-4 sm:pr-8 drop-shadow-[4px_4px_0_black] md:drop-shadow-[5px_5px_0_black]"
+              className="flex h-14 w-full items-center justify-between bg-primary pl-6 pr-4 drop-shadow-[4px_4px_0_black] sm:h-[77px] sm:pl-10 sm:pr-8 md:drop-shadow-[5px_5px_0_black] lg:w-[422px]"
             >
-              <span className="text-white font-bold sm:text-2xl md:text-[26px] tracking-wider">
+              <span className="font-bold tracking-wider text-white sm:text-2xl md:text-[26px]">
                 {t.BUTTON_LABEL}
               </span>
-              <div className="relative w-7 sm:w-[45px] pt-1">
+              <div className="relative w-7 pt-1 sm:w-[45px]">
                 <Image
                   src="/svg/arrow-right.svg"
                   height={29}
